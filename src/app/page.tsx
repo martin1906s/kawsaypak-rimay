5e0f00b32e94610dd7ca7fde6247326db18c11b4
martin1906s <martin.simbana007@gamil.com>
2025-05-30 13:27:15 -0500

    Cambio de diseño del header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,17 @@ import Logotipo from "../../components/isotype";
 export default function Home() {
   return (
     <header className="header">
-      <div className="isotipo">
-        <Logotipo />
-      </div>
-      <div className="nav-titles">
+      <div className="nav">
         <Navigation />
-        <h1 className="title">KAWSAYPAK RIMAY</h1>
-        <h6 className="slogan"><i className="slogan-i">- Yachaykunata tinkuchispa, Ayllu llaqtakunata tikraspa -</i></h6>
+      </div>
+      <div className="titles-isotipo">
+        <div className="titles">
+          <h1 className="title">KAWSAYPAK RIMAY</h1>
+          <h6 className="slogan"><i className="slogan-i">- Yachaykunata tinkuchispa, Ayllu llaqtakunata tikraspa -</i></h6>
+        </div>
+        <div className="logo">
+          <Logotipo />
+        </div>
       </div>
     </header>
   );
