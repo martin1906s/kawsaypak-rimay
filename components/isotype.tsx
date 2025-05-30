d903a1fa9fa3b4cc631137f4b6679d5f10e12a04
martin1906s <martin.simbana007@gamil.com>
2025-05-30 00:55:16 -0500

    Imagen del isotipo añadida

---
+++ components/isotype.tsx
@@ -0,0 +1,11 @@
+
+import Image from "next/image";
+export default function Logotipo() {
+    return (
+        <Image
+            src='/isotipo.png'
+            alt="logotipo kawsaypak rimay"
+            width={250}
+            height={200} />
+    )
+}
