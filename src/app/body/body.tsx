4c74add1b709f5492b9b735da2478befa0d255fd
martin1906s <martin.simbana007@gamil.com>
2025-06-05 16:42:59 -0500

    Diseño de video e informacion

--- src/app/body/body.tsx
+++ src/app/body/body.tsx
@@ -1,10 +1,49 @@
-'use client'
-export default function Body() {
+'use client';
+import { useEffect, useRef } from 'react';
+
+export default function IntroSection() {
+    const ref = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const element = ref.current;
+        if (!element) return;
+
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add('active');
+                    } else {
+                        entry.target.classList.remove('active');
+                    }
+                });
+            },
+            { threshold: 0.1 }
+        );
+
+        observer.observe(element);
+
+        return () => {
+            if (element) observer.unobserve(element);
+        };
+    }, []);
+
     return (
-        <main>
+        <main className="body-intro" ref={ref}>
             <div className="intro-video">
-                <p>En Kawsaypak Rimay, el futuro lo creamos hoy. Jóvenes programadores, saberes indígenas y tecnología se fusionan para transformar Imantag desde sus raíces. ¡Porque cuando el conocimiento se conecta con el corazón, todo es posible!</p>
-                <video src="/video/Kawsaypak Rimay.mp4" autoPlay muted playsInline controls></video>
+                <div className="intro">
+                    <p>En Kawsaypak Rimay, el futuro se construye hoy. Jóvenes programadores, saberes indígenas y tecnología se unen para revitalizar Imantag desde sus raíces, conectando mentes y corazones para crear un impacto sin fronteras.</p>
+                </div>
+                <div className="video-intro">
+                    <div className="hojas-container">
+                        <img src="/img/hoja.png" alt="Hoja" className="hoja hoja-izquierda" />
+                        <img src="/img/hoja.png" alt="Hoja" className="hoja hoja-centro" />
+                        <img src="/img/hoja.png" alt="Hoja" className="hoja hoja-derecha" />
+                    </div>
+                    <div className="video-container">
+                        <video src="/video/Kawsaypak Rimay.mp4" autoPlay muted playsInline controls></video>
+                    </div>
+                </div>
             </div>
         </main>
     );
