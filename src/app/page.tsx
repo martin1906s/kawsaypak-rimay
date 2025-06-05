5be4092bcbde89ae1277ea1516480a2fd3fe8684
martin1906s <martin.simbana007@gamil.com>
2025-06-05 12:54:42 -0500

    Texto introductorio y video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Header from "./header/header";
+import Body from "./body/body";
 
 export default function Home() {
   return (
-    <Header />
+    <main>
+      <Header />
+      <Body />
+    </main>
   );
 }
